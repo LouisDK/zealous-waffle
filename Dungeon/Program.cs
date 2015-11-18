c39eaae833bffca81defa91a4b1adc225c8b3c4d
Louis de Klerk <louisdeklerk@live.co.za>
2015-11-18 10:41:16 +0200

    Lesson 3 added

---
+++ Dungeon/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var House = new List<Room>();
+            House.Add(new Room()
+            {
+                RoomID = 1,
+                RoomTitle = "Entrance",
+                RoomDescription = "The door is open. Strangers walking by is beckoned to enter...",
+                PreviousRoomID = 0,
+                ListOfNextRooms = new List<int>() { 2 }
+            });
+
+            House.Add(new Room()
+            {
+                RoomID = 2,
+                RoomTitle = "Hallway",
+                RoomDescription = "The hallway leads to other rooms...",
+                PreviousRoomID = 1,
+                ListOfNextRooms = new List<int>() { 3, 4 }
+            });
+
+            House.Add(new Room()
+            {
+                RoomID = 3,
+                RoomTitle = "Study",
+                RoomDescription = "Dusty study. Unused...",
+                PreviousRoomID = 2
+            });
+
+            House.Add(new Room()
+            {
+                RoomID = 4,
+                RoomTitle = "Kitchen",
+                RoomDescription = "Smelling good!",
+                PreviousRoomID = 2
+            });
+
+            Room CurrentRoom = House.Where(q => q.RoomID == 2).First();
+
+            Console.WriteLine("Now in: " + CurrentRoom.RoomTitle);
+            Console.WriteLine("     " + CurrentRoom.RoomDescription);
+            foreach (var item in CurrentRoom.ListOfNextRooms)
+            {
+                var nextRoom = House.Where(q => q.RoomID == item).First();
+                Console.WriteLine("    Next available room: " + nextRoom.RoomTitle + " (" + nextRoom.RoomID + ")");
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Room
+    {
+        public Room()
+        {
+            this.ListOfNextRooms = new List<int>();
+        }
+
+        public int RoomID { get; set; }
+
+        public string RoomTitle { get; set; }
+        public string RoomDescription { get; set; }
+
+        public int PreviousRoomID { get; set; }
+
+        public List<int> ListOfNextRooms { get; set; }
+
+    }
+
+
+}
---
+++ Lesson3Loops/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 5;
+            //int[] myNumbers = new int[x];
+
+            //myNumbers[0] = 21;
+            //myNumbers[1] = 22;
+            //myNumbers[2] = 23;
+            //myNumbers[3] = 24;
+            //myNumbers[4] = 25;
+
+            var myStopwatch = new System.Diagnostics.Stopwatch();
+            myStopwatch.Start();
+
+            var myNumbers = new List<int>() ;
+
+            for (int j = 0; j < 10; j++)
+            {
+                myNumbers.Add(j);
+            }
+
+            myStopwatch.Stop();
+            int test = -5;
+
+            //Console.WriteLine("There is now " + myNumbers.Count + " items in my list");
+
+            //Console.WriteLine("   time taken: " + myStopwatch.Elapsed.Milliseconds);
+
+            //myNumbers.Remove(55);
+            //myNumbers.RemoveAt(1);
+            //myNumbers.Insert(3, 77);
+
+            //Console.WriteLine("Here we see the For loop:");
+            //for (int i = 0; i < myNumbers.Count; i++)
+            //{
+            //    Console.WriteLine("Current iteration: " + i + ". Current Value of Array at that position: " + myNumbers[i]);
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("Here we see the Foreach loop:");
+
+            //foreach (var myNumber in myNumbers)
+            //{
+            //    Console.WriteLine("Current value is: " + myNumber);
+            //}
+
+          
+
+
+            var People = new List<Person>();
+
+            People.Add(new Person() { Firstname = "Louis", Lastname = "De Klerk", Age = 39, Email = "Louis@inobits.com", ManagerEmail = "Hud@inobits.com" });
+            People.Add(new Person() { Firstname = "John", Lastname = "Barsby", Age = 62, Email = "John@inobits.com", ManagerEmail = "Hud@inobits.com" });
+            People.Add(new Person() { Firstname = "Hud", Lastname = "Krause", Age = 45, Email = "Hud@inobits.com" });
+
+            var CurrentPerson = People.First();
+            var CurrentManager = People.Where(q => q.Email == CurrentPerson.ManagerEmail).First();
+
+            Console.WriteLine(" current person " + CurrentPerson.ToString() + " 's manager is " + CurrentManager.ToString());
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public string ManagerEmail { get; set; }
+
+        public override string ToString()
+        {
+            return Firstname + " " + Lastname + " (" + Email + ")";
+        }
+    }
+
+
+
+
+}
